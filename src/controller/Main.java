6fcf44f66e092f64fdea3a3889f913fd05bb3273
GrafZ3pp3lin <jojo98.hannes@gmail.com>
2018-06-22 11:16:52 +0200

    Hello World

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,9 +1,21 @@
 package controller;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Timer;
+
 public class Main extends Application {
+
+    private Stage primaryStage;
+    private Parent startLayout;
+    private Parent gameLayout;
 
     public static void main(String[] args) {
         launch(args);
@@ -11,6 +23,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        System.out.println("Hello World");
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle("Dame");
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+        initRootLayout();
+        primaryStage.show();
+    }
+
+    private void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/StartPane.fxml"));
+            startLayout = loader.load();
+            primaryStage.setScene(new Scene(startLayout));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
