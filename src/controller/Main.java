affbb5ff73bd30146e244d286df0c9eaf69aabc9
GrafZ3pp3lin <jojo98.hannes@gmail.com>
2018-06-22 11:37:42 +0200

    Create GUI Classes

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class Main extends Application {
     private Stage primaryStage;
     private Parent startLayout;
     private Parent gameLayout;
+    private BorderPane menuLayout;
 
     public static void main(String[] args) {
         launch(args);
@@ -27,18 +29,52 @@ public class Main extends Application {
         primaryStage.setTitle("Dame");
         primaryStage.setOnCloseRequest(e -> Platform.exit());
         initRootLayout();
+        loadStartLayout();
+        loadGameLayout();
+        setStartLayout();
         primaryStage.show();
     }
 
     private void initRootLayout() {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/StartPane.fxml"));
-            startLayout = loader.load();
-            primaryStage.setScene(new Scene(startLayout));
+            loader.setLocation(Main.class.getResource("view/MenuPane.fxml"));
+            menuLayout = loader.load();
+            primaryStage.setScene(new Scene(menuLayout));
         }
         catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private void loadStartLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/StartPane.fxml"));
+            startLayout = loader.load();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setStartLayout() {
+        menuLayout.setCenter(startLayout);
+    }
+
+    private void loadGameLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/GamePane.fxml"));
+            gameLayout = loader.load();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setGameLayout() {
+        menuLayout.setCenter(gameLayout);
+    }
+
 }
